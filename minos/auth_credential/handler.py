795348950e517b6171c65ee3b11b973455658bf1
Vladyslav Fenchak <vladyslav.fenchak@clariteia.com>
2022-01-18 13:51:28 +0100

    Update handler.py

--- minos/auth_credential/handler.py
+++ minos/auth_credential/handler.py
@@ -3,11 +3,16 @@ import logging
 from datetime import (
     datetime,
 )
+from uuid import uuid4
+
 from aiohttp import (
     web,
 )
 from sqlalchemy.orm import (
     sessionmaker,
+)
+from sqlalchemy import (
+    exc,
 )
 from .cryptography.cryptography import (
     AuthCrypto,
@@ -34,18 +39,23 @@ async def add_credentials(request: web.Request) -> web.Response:
     s = Session()
 
     now = datetime.now()
+    uuid = uuid4()
     credential = Credential(
+        uuid=uuid,
         username=content["username"],
         password=AuthCrypto().encrypt_password(content["password"]),
         created_at=now,
         updated_at=now,
     )
 
-    s.add(credential)
-    s.commit()
+    try:
+        s.add(credential)
+        s.commit()
+    except exc.IntegrityError:
+        return web.json_response(status=500, text="Username is already taken.")
 
     s.close()
-    return web.json_response({})
+    return web.json_response(str(uuid))
 
 
 async def validate_credentials(request: web.Request) -> web.Response:
